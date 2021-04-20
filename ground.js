fb3246103e611317f4c341174c46070208369b0d
KAVIDARSHINI <76558960+KAVIDARSHINI@users.noreply.github.com>
2021-04-20 15:20:18 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -7,11 +7,12 @@ class Ground{
       this.w = w; 
       this.h = h;
       World.add(world, this.body);
+         this.color = color(random(0,200), random(0,200), random(0,200));
     }
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("white");
+      fill(this.color);
     rect(pos.x, pos.y, this.w,this.h);
     }
   };
